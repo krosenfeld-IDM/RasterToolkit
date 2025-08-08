90703455a5485724c7dd1621c13e2b1be5dcdd47
Kurt Frey <kfrey@idmod.org>
2025-08-08 19:23:35 +0000

    Bump version

--- rastertoolkit/version.py
+++ rastertoolkit/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = "0.3.14"
-__versiondate__ = "2025-07-13"
+__version__ = "0.3.15"
+__versiondate__ = "2025-08-08"
 __license__ = f"rastertoolkit {__version__} ({__versiondate__}) — © 2022-2025 by IDM"
