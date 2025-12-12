56be70b3476d5bd978cbc64746c923bdc6018d41
Kurt Frey <kfrey@idmod.org>
2025-12-12 11:48:24 -0800

    Simplify version changes

--- rastertoolkit/version.py
+++
@@ -1,9 +0,0 @@
-"""
-Version and license information.
-"""
-
-__all__ = ["__version__", "__versiondate__", "__license__"]
-
-__version__ = "0.3.24"
-__versiondate__ = "2025-11-24"
-__license__ = f"rastertoolkit {__version__} ({__versiondate__}) — © 2022-2025 by IDM"
